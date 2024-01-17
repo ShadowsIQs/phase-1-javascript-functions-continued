5ad6b69eaa447c615cfc138e4516520fd4a99ca7
Ryan <griffinryan77@gmail.com>
2024-01-16 23:45:56 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// code your solution here
+function saturdayFun(word1 = "roller-skate"){
+    return (`This Saturday, I want to ${word1}!`)
+}
+saturdayFun()
+
+function mondayWork(word2 = "go to the office"){
+    return (`This Monday, I will ${word2}.`)
+}
+mondayWork()
+
+// function wrapAdjective(){
+// const wrap = wrapAdjective(`*`) <--- can't use because it will make a loop hole
+// can't call a funcion in a function.
+// return function (msg = "special"){
+// const hey = wrap (`You are ${msg}!`)
+//         return hey
+//     }
+// }
+
+function wrapAdjective(symbol = "*"){
+     return function (msg = "special"){
+                const hey =  (`You are ${symbol}${msg}${symbol}!`)
+                return hey
+            }
+}
+
+console.log(wrapAdjective("%")("a dedicated programmer"))
